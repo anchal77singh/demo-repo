990606ddd5495e35c0bb27e8da21ebe5c7f0895d
anchal77singh <anchal77singh@gmail.com>
2021-04-20 10:10:57 +0530

    first-commit

---
+++ java-project-demo/src/com/sapient/DemoJava.java
@@ -0,0 +1,13 @@
+package com.sapient;
+
+import java.util.Date;
+
+public class DemoJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Today's Date is: "+new Date());
+
+	}
+
+}
